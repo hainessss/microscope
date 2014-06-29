0bf282cc56d2e656fa6b7238975886d0ffd233bb
alex haines <hainesaxiv@gmail.com>
2014-06-29 00:27:13 -0700

    added basic posts list templates

---
+++ client/views/posts/posts_lists.js
@@ -0,0 +1,21 @@
+var postsData = [
+  {
+    title: 'Introducing Telescope',
+    author: 'Sacha Greif',
+    url: 'http://sachagreif.com/introducing-telescope/'
+  },
+  {
+    title: 'Meteor',
+    author: 'Bob Sacamano',
+    url: 'http://www.meteor.com'
+  },
+  {
+    title: 'The Meteor Book',
+    author: 'Tom Coleman',
+    url: 'http://themeteorbook.com'
+  }
+];
+
+Template.postsList.helpers({
+  posts: postsData
+});
