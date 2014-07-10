83797888d990d7d7ec508744148cffc38db8f887
alex haines <hainesaxiv@gmail.com>
2014-07-09 21:04:08 -0700

    created first collection

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,22 +1,17 @@
-var postsData = [
-  {
+if (Posts.find().count() === 0) {
+  Posts.insert({
     title: 'Introducing Telescope',
     author: 'Sacha Greif',
     url: 'http://sachagreif.com/introducing-telescope/'
-  },
-  {
+  });
+Posts.insert({
     title: 'Meteor',
-    author: 'Bob Sacamano',
-    url: 'http://www.meteor.com'
-  },
-  {
+    author: 'Tom Coleman',
+    url: 'http://meteor.com'
+  });
+Posts.insert({
     title: 'The Meteor Book',
     author: 'Tom Coleman',
     url: 'http://themeteorbook.com'
-  }
-];
-
-Template.postsList.helpers({
-  posts: postsData
-});
-
+  });
+}
